7b3b72eedabbb2a19808b695f64f39ddb410c7a3
Eric Hackman <kwesihackman@gmail.com>
2019-12-20 17:35:25 +0000

    finished the 404 page design and link page to the home route

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import image from '../images/not_found.svg'
 
 const NotFound: React.FC = () => {
+    const history = useHistory()
     return (
-        <div>
-            404
+        <div className="mt-5 d-flex justify-content-center">
+            <div className="w-60">
+                <img src={image} alt="Page not found"/>
+                <h3 className="text-white text-center cursor-pointer mt-5" onClick={() => history.push('/')}>
+                    <small>Sometimes we get lost in the music. Wanna go home ? </small> <br/>
+                Take Me Home
+            </h3>
+            </div>
+           
         </div>
     )
 }
